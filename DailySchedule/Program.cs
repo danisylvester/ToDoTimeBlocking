84d646ac69e4ba95ed4be339d0896c925bafbeb0
danisylvester <maria.sylvester10@gmail.com>
2020-09-08 17:07:33 -0500

    Created project.

---
+++ DailySchedule/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DailySchedule
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+
+    }
+}
---
+++ DailySchedule/Task.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DailySchedule
+{
+    class Task
+    {
+        public List<string> TaskName { get; set; }
+         
+
+
+    }
+}
